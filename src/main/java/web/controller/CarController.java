ce8d1753f3f21af7a72255a65b43df9b05379ac8
KostiantynDudchenko <dke130987@gmail.com>
2023-01-27 17:01:32 +0200

    deleted @Autowired annotation

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,11 +1,9 @@
 package web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.dao.CarDAO;
 import web.service.CarService;
 
 
@@ -13,7 +11,6 @@ import web.service.CarService;
 public class CarController {
     private final CarService carService;
 
-    @Autowired
     protected CarController(CarService carService) {
         this.carService = carService;
     }
