3cecf16097c284a913534688bbba026fc84e41b9
KostiantynDudchenko <dke130987@gmail.com>
2023-02-02 16:18:45 +0200

    Repository without @Transaction

--- src/main/java/web/dao/UserDAOImpl.java
+++ src/main/java/web/dao/UserDAOImpl.java
@@ -15,25 +15,21 @@ public class UserDAOImpl implements UserDAO {
     private EntityManager entityManager;
 
     @Override
-    @Transactional
     public List<User> getUsers() {
         return entityManager.createQuery("select e from User e").getResultList();
     }
 
     @Override
-    @Transactional
     public void save(User user) {
         entityManager.persist(user);
     }
 
     @Override
-    @Transactional
     public User show(int id) {
         return entityManager.find(User.class, id);
     }
 
     @Override
-    @Transactional
     public void update(int id, User user) {
         User userToBeUpdate = show(id);
         userToBeUpdate.setName(user.getName());
@@ -43,7 +39,6 @@ public class UserDAOImpl implements UserDAO {
     }
 
     @Override
-    @Transactional
     public void delete(int id) {
         entityManager.remove(entityManager.contains(show(id)) ? show(id) : entityManager.merge(show(id)));
     }
